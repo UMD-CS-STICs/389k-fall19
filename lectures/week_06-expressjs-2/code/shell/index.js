dea9b0587744f16b408849f32933570d62a5f0be
Robert Choe <robertchoe@Roberts-MacBook-Pro.local>
2019-10-10 21:30:25 -0400

    survey

--- lectures/week_06-expressjs-2/code/shell/index.js
+++ lectures/week_06-expressjs-2/code/shell/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res) {
-    res.send('<a src="https://docs.google.com/forms/d/e/1FAIpQLSeVLeEgIPyCUoYP_6PQTy0oeF9rJXZmYo7FKvRI4fyZX-0RXg/viewform?usp=sf_link">Click Me!</a>');
+    res.send('<a href="https://docs.google.com/forms/d/e/1FAIpQLSeVLeEgIPyCUoYP_6PQTy0oeF9rJXZmYo7FKvRI4fyZX-0RXg/viewform?usp=sf_link">Click Me!</a>');
 });
 
 app.get('/factorial', function(req, res) {
