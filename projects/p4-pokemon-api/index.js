d5ebe0d9260dd69f5edcc3a1709c0f9ca3cca035
Robert Choe <robertchoe@Roberts-MacBook-Pro.local>
2019-10-14 20:46:55 -0400

    midterm added

--- projects/p4-pokemon-api/index.js
+++ projects/p4-pokemon-api/index.js
@@ -22,20 +22,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", function(req, res) {
-    // HINT: 
-    // var contents = "";
-    // _.each(_DATA, function(i) {
-    //     contents += `<tr><td>1</td><td><a href="/pokemon/1">Nelson</a></td></tr>\n`;
-    // })
-    // var html = `<html>\n<body>\n<table>CONTENTS</table>\n</body>\n</html>`;
-    // res.send(html);
-    res.send("UNIMPLEMENTED ENDPOINT");
+   var contents = "";
+   _.each(_DATA, function(i) {
+       contents += `<tr><td>1</td><td><a href="/pokemon/1">Nelson</a></td></tr>\n`;
+   })
+   var html = `<html>\n<body>\n<table>CONTENTS</table>\n</body>\n</html>`;
+   res.send(html);
+    //res.send("UNIMPLEMENTED ENDPOINT");
 });
 
 app.get("/pokemon/:pokemon_id", function(req, res) {
     // HINT : 
     // <tr><td>${i}</td><td>${JSON.stringify(result[i])}</td></tr>\n`;
-    res.send("UNIMPLEMENTED ENDPOINT");
+    //res.send("UNIMPLEMENTED ENDPOINT");
+    var _id = req.params.pokemon_id;
+
+    var result = _.find(_DATA, function(i){
+      return i.id == _id;
+    });
+    res.render("info", {pokemonInfo: result});
 });
 
 app.get("/pokemon/image/:pokemon_id", function(req, res) {
@@ -51,27 +56,126 @@ app.get("/api/id/:pokemon_id", function(req, res) {
 });
 
 app.get("/api/evochain/:pokemon_name", function(req, res) {
-    res.send("UNIMPLEMENTED ENDPOINT");
+    //res.send("UNIMPLEMENTED ENDPOINT");
+    var curr_pokemon = req.params.pokemon_name;
+    var result = [];
+    _.each(_DATA, function(i) {
+      if(curr_pokemon == i.name){
+				console.log(i) 
+      }
+    });
+   res.send(result);
 
 });
 
 app.get("/api/type/:type", function(req, res) {
-    res.send("UNIMPLEMENTED ENDPOINT");
+    //res.send("UNIMPLEMENTED ENDPOINT");
+    var _type = req.params.type;
+    var results = [];
+    _.each(_DATA, function(i){
+      if(i.type[0] == _type){
+        results.push(i.name);
+      };
+    });
+    if (!result) {
+      return res.json({});
+    }
+    res.json(results);
 });
 
 app.get("/api/type/:type/heaviest", function(req, res) {
-    res.send("UNIMPLEMENTED ENDPOINT");
+    //res.send("UNIMPLEMENTED ENDPOINT");
+    var _type = req.params.type;
+    if(_type == null){
+      return res.json({});
+    }
+    var result = [];
+    var y =[];
+  
+  
+    var x = _.filter(_DATA, function(i){
+      return i.type == _type;
+    });
+  
+    _.each(x, function(j){
+      j.weight = parseInt(j.weight.replace(/\D/g,''));
+      y.push(_.pick(j,'name','weight'));
+    });
+
+    // result = _.max(y, function(k){
+    //     return k.weight;
+    //   });
+    
+  
+    if (!result) {
+      return res.json({});
+    }
+  
+    res.json(result);
 });
 
 app.post("/api/weakness/:pokemon_name/add/:weakness_name", function(req, res) {
     // HINT: 
     // Use `pokeDataUtil.saveData(_DATA);`
-    res.send("UNIMPLEMENTED ENDPOINT");
+    //res.send("UNIMPLEMENTED ENDPOINT");
+    var name = req.params.pokemon_name;
+    var weakness = req.params.weakness_name;
+    var result = [];
+    var bool = true;
+    if(!name){
+        return res.json({});
+    }
+    var nameValue = _.find(_DATA, function(x){
+        return name == x.name;
+     });
+
+    for(var i = 0; i < nameValue.weaknesses.length; i++){
+        if(weakness == nameValue.weaknesses[i]){
+            bool = false;
+        }
+    }
+
+  if(bool == true){
+     nameValue.weaknesses.push(weakness);
+     pokeDataUtil.saveData(_DATA);
+   }
+
+  _.each(_DATA, function(i){
+    result.push(_.pick(i,'name','weaknesses'));
+  });
+
+  res.json(result);
 });
 
 app.delete("/api/weakness/:pokemon_name/remove/:weakness_name", function(req, res) {
-    res.send("UNIMPLEMENTED ENDPOINT");
+    //res.send("UNIMPLEMENTED ENDPOINT");
+  var name = req.params.pokemon_name;
+  var weakness = req.params.weakness_name;
+  var result = [];
+  
+  if(!name){
+    return res.json({});
+  }
+  var nameValue = _.find(_DATA, function(x){
+    return name == x.name;
+  });
+
+  for(var i = 0; i < nameValue.weaknesses.length; i++){
+    if(weakness == nameValue.weaknesses[i]){
+      nameValue.weaknesses.pop(weakness);
+      pokeDataUtil.saveData(_DATA);
+    }
+  }
+
+  _.each(_DATA, function(i){
+    result.push(_.pick(i,'name','weaknesses'));
+  });
+ 
+  res.json(result);
+
+  
 });
+
 
 
 // Start listening on port PORT
