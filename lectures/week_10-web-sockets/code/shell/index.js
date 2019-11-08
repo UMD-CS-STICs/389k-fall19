69f06d469a07813dde18aaec110e3c3e373822db
Robert Choe <robertchoe@wireless-10-105-88-244.umd.edu>
2019-11-07 23:46:35 -0500

    week 10 modifications

--- lectures/week_10-web-sockets/code/shell/index.js
+++ lectures/week_10-web-sockets/code/shell/index.js
@@ -4,6 +4,17 @@ var bodyParser = require('body-parser');
 var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var dotenv = require('dotenv');
+/*
+Allow server to use Socket.io
+1. Switch to http server, but pass in express app in the function handler (so we can still use app to route all our URLs)
+2. Use socket io to connect to the web server
+3. Change app.listen to http.listen
+4. Use socket io to handle new connections to our server -> io.on('connection' ....){}
+*/
+
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 dotenv.load();
 
 app.use(bodyParser.json());
@@ -29,21 +40,45 @@ app.get('/movies', function(req, res) {
         return res.render('movies', { movies: movies });
     });
 });
+/*  ===================TASK 2====================
+    Task 2 (SERVER SIDE): Handle a new movie post
+    Steps to complete task 2:
+        1. Handle POST request
+        2. Create new movie
+        3. Save new movie to database
+        4. Emit new movie message to all clients currently connected
+*/
 
+//Task 2 - Step 1: Handle POST request
 app.post('/movies', function(req, res) {
-    var genre = req.body.genre;
-    var title = req.body.title;
-    var movie = new Movie({
-        title: title,
-        genre: genre
-    });
-    movie.save(function(err) {
-        if (err) throw err;
-        io.emit('new movie', movie);
-        return res.send('Done!');
+    //Task 2 - Step 2: Create new movie
+    //Task 2 - Step 3 & 4: Save new movie to database, emit new movie message to clients
+});
+
+//An event listener to listen for client connecting to our server
+io.on('connection', function(socket) {
+    console.log('NEW connection');
+    /*
+        - Handle listening for "messages" 
+            -> socket.on(message, function(msg)){...}
+        - Sending out messages to ALL clients currently connected
+            -> io.emit(message, contentOfMessage)
+    */
+
+    /*        ===================TASK 1====================
+        TASK 1 (SERVER END): Handle a new chat message from a client 
+        Steps to compelete task 1 on server end.
+            1. Listen for a new chat message
+            2. Emit new chat message to all clients currently connected
+    */
+
+    //Task 1 - Step 1: Listen for a new chat message
+        //Task 2 - Step 2: Emit new chat message to all clients currently connected
+    socket.on('disconnect', function() {
+        console.log('User has disconnected');
     });
 });
 
-app.listen(3000, function() {
+http.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
